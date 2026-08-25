4e5dcde5c53db0b4d92685384ee6b859cd4da8d6
syedshuaib18 <syedshuaibt7@gmail.com>
2026-08-25 12:50:47 +0530

    Initial Spring Boot setup

---
+++ ev-roadtrip-optimizer/src/main/java/com/example/ev_roadtrip_optimizer/EvRoadtripOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ev_roadtrip_optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvRoadtripOptimizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvRoadtripOptimizerApplication.class, args);
+	}
+
+}
---
+++ ev-roadtrip-optimizer/src/test/java/com/example/ev_roadtrip_optimizer/EvRoadtripOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ev_roadtrip_optimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvRoadtripOptimizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
